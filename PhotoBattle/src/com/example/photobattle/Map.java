233962315c030c9f66a244644a44b0bd2aa4fe94
gabsSP1 <gabs.dupont@gmail.com>
2016-02-11 10:38:19 +0100

    a

--- PhotoBattle/src/com/example/photobattle/Map.java
+++ PhotoBattle/src/com/example/photobattle/Map.java
@@ -1,6 +1,9 @@
 package com.example.photobattle;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Vector;
 
 import android.app.Activity;
@@ -11,34 +14,82 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.Layout;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
+import android.widget.AbsListView.MultiChoiceModeListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 //a
 public class Map extends Activity {
-	ScrollView s;
-	LinearLayout l;
+	ListView s;
+	HashMap<Integer, Bitmap> fichierJpeg;
+	ActionMode mActionMode;
+	int f;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		f=0;
 		super.onCreate(savedInstanceState);
 		/*getWindow().setFormat(PixelFormat.TRANSLUCENT);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);*/
 		setContentView(R.layout.activity_map);
-		s=(ScrollView) findViewById(R.id.scroll);
-		l=(LinearLayout) this.findViewById(R.id.l);
+		s=(ListView) findViewById(R.id.scroll);
+		/*s.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+		s.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+
+		    @Override
+		    public void onItemCheckedStateChanged(ActionMode mode, int position,
+		                                          long id, boolean checked) {
+		        // Here you can do something when items are selected/de-selected,
+		        // such as update the title in the CAB
+		    }
+
+		    @Override
+		    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+		        // Respond to clicks on the actions in the CAB
+		        
+		                return false;
+		        
+		    }
+
+		    @Override
+		    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+		        // Inflate the menu for the CAB
+		        MenuInflater inflater = mode.getMenuInflater();
+		        inflater.inflate(R.menu.menu_app, menu);
+		        return true;
+		    }
+
+		    @Override
+		    public void onDestroyActionMode(ActionMode mode) {
+		        // Here you can make any necessary updates to the activity when
+		        // the CAB is removed. By default, selected items are deselected/unchecked.
+		    }
+
+		    @Override
+		    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+		        // Here you can perform updates to the CAB due to
+		        // an invalidate() request
+		        return false;
+		    }
+		});*/
+		
+		
+		
 		//Récupération des fichiers jpeg dans le dossier
 		
 		File f2 = new File(Environment.getExternalStorageDirectory() +
 	                File.separator + "photoBattle");
 		 File[] fichiers = f2.listFiles();
 		 
-		 int f=0;
-		 Vector<File> fichierJpeg=new  Vector<File>(15);
+		 fichierJpeg=new  HashMap<Integer, Bitmap>();
 		 for(int i=0; i<fichiers.length;i++)
 		 {
 			 
@@ -53,21 +104,27 @@ public class Map extends Activity {
 			}
 			 if(ext.equals("gpj"))
 			 {
-				 fichierJpeg.add(fichiers[i]);
 				 f++;
 				 LayoutParams lparams = new LayoutParams(
 				 LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 				 ImageView tv=new ImageView(this);
 				 tv.setLayoutParams(lparams);
 				 Bitmap myBitmap = BitmapFactory.decodeFile(fichiers[i].getAbsolutePath());
-			     tv.setImageBitmap(myBitmap);
-				 this.l.addView(tv);
+				 fichierJpeg.put(f,myBitmap);
+				 //this.s.addView(tv);
 				 
 			 }
 			 
 		 }
-		 
-		
+		 LayoutParams lparams = new LayoutParams(
+		LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		 TextView t=new TextView(this);
+		 t.setLayoutParams(lparams);
+		 t.setText(Integer.toString(f));
+		// s.addView(t);
+		 Bitmap list2[] = new Bitmap[fichierJpeg.size()];
+		 ArrayAdapterItem adapter =new ArrayAdapterItem(this, R.layout.image_view_layout, fichierJpeg.values().toArray(list2));
+		 s.setAdapter(adapter);
 		
 	}
 
@@ -79,14 +136,57 @@ public class Map extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (id) {
+        case R.id.Delete:
+        	if (mActionMode != null) {
+                return false;
+            }
+
+            // Start the CAB using the ActionMode.Callback defined above
+            mActionMode = this.startActionMode(mActionModeCallback);
+            s.setSelected(true);
+
+        	break;
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+	    // Called when the action mode is created; startActionMode() was called
+	    @Override
+	    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+	        // Inflate a menu resource providing context menu items
+	        MenuInflater inflater = mode.getMenuInflater();
+	        inflater.inflate(R.menu.menu_action_bar, menu);
+	        return true;
+	    }
+
+	    // Called each time the action mode is shown. Always called after onCreateActionMode, but
+	    // may be called multiple times if the mode is invalidated.
+	    @Override
+	    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+	        return false; // Return false if nothing is done
+	    }
+
+	    // Called when the user selects a contextual menu item
+	    @Override
+	    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+	                return false;
+	        
+	    }
+
+	    // Called when the user exits the action mode
+	    @Override
+	    public void onDestroyActionMode(ActionMode mode) {
+	        mActionMode = null;
+	    }
+	};
 }
