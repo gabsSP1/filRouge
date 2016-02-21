f0a2e0478221cc7c3c9acf139c893b76d5735d1a
gabsSP1 <gabs.dupont@gmail.com>
2016-02-21 15:35:00 +0100

    a

--- PhotoBattle/src/com/example/photobattle/MenuApp.java
+++ PhotoBattle/src/com/example/photobattle/MenuApp.java
@@ -28,7 +28,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class MenuApp extends Activity{
-
+	Button b1;
 	File myDir;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,20 +41,9 @@ public class MenuApp extends Activity{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		setContentView(R.layout.activity_menu_app);
-		Button b2=(Button) findViewById(R.id.button2);
-		Button b3=(Button) findViewById(R.id.button3);
-		b2.setOnClickListener(new OnClickListener (){
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				dispatchTakePictureIntent();
-				
-			}
-			
-		});
+		b1=(Button) findViewById(R.id.button1);
 		
-		b3.setOnClickListener(new OnClickListener (){
+		b1.setOnClickListener(new OnClickListener (){
 
 			@Override
 			public void onClick(View v) {
@@ -76,61 +65,6 @@ public class MenuApp extends Activity{
 		return true;
 	}
 	
-	static final int REQUEST_TAKE_PHOTO = 1;
-
-	private void dispatchTakePictureIntent() {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    // Ensure that there's a camera activity to handle the intent
-	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-	        // Create the File where the photo should go
-	        File photoFile = null;
-	        try {
-	            photoFile = createImageFile();
-	        } catch (IOException ex) {
-	            // Error occurred while creating the File
-	        }
-	        // Continue only if the File was successfully created
-	        if (photoFile != null) {
-	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-	                    Uri.fromFile(photoFile));
-	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-	        }
-	    }
-	}
 	
-	String mCurrentPhotoPath;
-
-	
-	
-	private File createImageFile() throws IOException {
-	    // Create an image file name
-	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-	    String imageFileName = "JPEG_" + timeStamp + ".jpeg";
-       
-	    File myDir = new File(Environment.getExternalStorageDirectory() +
-                File.separator + "photoBattle"); //pour créer le repertoire dans lequel on va mettre notre fichier
-		Boolean success=true;
-		if (!myDir.exists()) 
-		{
-		success = myDir.mkdir(); //On crée le répertoire (s'il n'existe pas!!)
-		}
-		if (success)
-		{
-         
-		 File image = File.createTempFile(
-			        imageFileName,  /* prefix */
-			        ".jpg",         /* suffix */
-			        myDir      /* directory */
-			    );
-		 mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-		 return image;
-		}
-		else 
-		{
-			Log.e("TEST1","ERROR DE CREATION DE DOSSIER");
-		}
-		return null;
-			    
-	}
 	
 }
