8dce3403c339a0204c12bdfd7a5104f1497710fb
gab <gabriel.specq@insa-lyon.fr>
2016-02-29 21:48:36 +0100

    méthode de filtre de hugo ajouté dans l'activité map

--- app/src/main/java/com/example/photobattle/Map.java
+++ app/src/main/java/com/example/photobattle/Map.java
@@ -12,7 +12,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -37,6 +41,7 @@ public class Map extends Activity {
 	HashMap<String, Bitmap> thumbnail;
 	ActionMode mActionMode;
 	File currentSelectionFile;
+    private ImageView image;
 	ImageView imageSelected;
 	Button bDelete;
 	Button importPicture;
@@ -153,7 +158,7 @@ public class Map extends Activity {
 	
 	
 	static final int REQUEST_TAKE_PHOTO = 1;
-
+    //gère la prise de la photo
 	private void dispatchTakePictureIntent() {
 	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    // Ensure that there's a camera activity to handle the intent
@@ -191,6 +196,7 @@ public class Map extends Activity {
 		     if(nf.getTotalSpace()>20)
 		     {
 		    	 createThumbnail(nf.getAbsolutePath());
+                 createImageContours(nf.getAbsolutePath());
 		     }
 		     else
 		     {
@@ -201,7 +207,9 @@ public class Map extends Activity {
 	     loadList();
 	        
 	}
-	
+
+
+    //gère l'import de photo
 	 private void savebitmap(Uri targetUri) {
 	      OutputStream outStream = null;
 	      
@@ -214,13 +222,14 @@ public class Map extends Activity {
 	         outStream.flush();
 	         outStream.close();
 	         createThumbnail(file.getAbsolutePath());
+            createImageContours(file.getAbsolutePath());
 	      } catch (Exception e) {
 	         e.printStackTrace();
 	      }
 
 	   }
 	 
-	 
+	 //create a file on the indicated path with te indicated name
 	 private File createImageFile(String rPath, String imageFileName) throws IOException {
 		    // Create an image file name
 		    File myDir = new File(Environment.getExternalStorageDirectory() +
@@ -240,7 +249,9 @@ public class Map extends Activity {
 			return null;
 				    
 		}
-	 
+
+
+    //Create a thumbnail in the directory /thumbnail of the file indicated by the parameter path
 	 void createThumbnail(String path)
 	 {
 		 Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
@@ -267,7 +278,9 @@ public class Map extends Activity {
 		}
          
 	 }
-	 
+
+
+    //Create a directory to the indicated path
 	 boolean createDirectory(String rPath)
 	 {
 		 File myDir = new File(Environment.getExternalStorageDirectory() +
@@ -279,14 +292,18 @@ public class Map extends Activity {
 			}
 		 return success;
 	 }
-	 
+
+
+    //Create a name of file that doesn't already exists
 	 String createPictureName()
 	 {
 		 String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 		 String imageFileName = "JPEG_" + timeStamp + ".jpg";	
 		 return imageFileName;
 	 }
-	 
+
+
+    //resize a bitmap (bad quality, ideal to make light thumbnail image of a bigger picture)
 	 public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
 		    int width = bm.getWidth();
 		    int height = bm.getHeight();
@@ -303,7 +320,9 @@ public class Map extends Activity {
 		    bm.recycle();
 		    return resizedBitmap;
 		}
-	 
+
+
+    //Load the list of the picture
 	 void loadList()
 	 {
 		 s.removeAllViewsInLayout();
@@ -353,15 +372,154 @@ public class Map extends Activity {
 		 MonAdaptateurDeListe adapter =new MonAdaptateurDeListe(this, thumbnail.keySet().toArray(list3), thumbnail.values().toArray(list2));
 		 s.setAdapter(adapter);
 	 }
-	 
-	 void restart()
-	 {
-		 startActivity(getIntent());
-		 finish(); 
-	 }
+
 	public void onRestart(){
 		super.onRestart();
 		loadList();
 	}
-	 
+
+
+
+	//Partie de Hugo Monyac
+
+    void createImageContours(String path)
+    {
+        Bitmap bm=BitmapFactory.decodeFile(path);
+        bm=grayScale(bm);
+        bm=chgtoBandW(bm);
+        OutputStream outStream = null;
+
+        File file;
+        try {
+            file = createImageFile("photoBattle" +
+                    File.separator+ "Contours", new File(path).getName());
+            outStream = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.JPEG, 80, outStream);
+            outStream.flush();
+            outStream.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
+	Bitmap chgtoBandW(Bitmap img){
+		//img.setImageBitmap(source)
+		Bitmap mapnew= Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+		int color = 0;
+		int[][] bmx= new int[img.getWidth()][img.getHeight()];
+		for(int i=0;i<img.getWidth();i++)
+		{
+			for(int j= 0;j<img.getHeight();j++)
+			{
+				int colorOfPixel = img.getPixel(i, j);
+				mapnew.setPixel(i,j,colorOfPixel);
+
+			}
+		}
+		mapnew = DoFullFilter(mapnew);
+        return mapnew;
+	}
+
+	public Bitmap DoHorizontalFilter(Bitmap BitmapGray){
+		int w, h, threshold;
+
+		h= BitmapGray.getHeight();
+		w= BitmapGray.getWidth();
+
+		threshold = 100;
+
+		Bitmap BitmapBiner = Bitmap.createBitmap(BitmapGray);
+
+		for(int x = 0; x < w-1; ++x) {
+			for(int y = 0; y < h; ++y) {
+
+				int pixel = BitmapGray.getPixel(x, y);
+				int pixelsuivant = BitmapGray.getPixel(x+1,y);
+
+				int gray = (Color.red(pixel) +Color.blue(pixel)+Color.green(pixel))/3;
+				int gNext = (Color.red(pixelsuivant) +Color.blue(pixelsuivant)+Color.green(pixelsuivant))/3;
+				if((gray-gNext)*(gray-gNext)< threshold){
+					BitmapBiner.setPixel(x, y, 0xFFFFFFFF);
+				} else{
+					BitmapBiner.setPixel(x, y, 0xFF000000);
+				}
+
+			}
+		}
+		return BitmapBiner;
+
+	}
+	public Bitmap DoFullFilter(Bitmap BitmapGray){
+		int w, h, threshold;
+
+		h= BitmapGray.getHeight();
+		w= BitmapGray.getWidth();
+
+		Bitmap BitmapBiner = Bitmap.createBitmap(BitmapGray);
+		Bitmap bHor = DoHorizontalFilter(BitmapGray);
+		Bitmap bVer = DoVerticalFilter(BitmapGray);
+		for(int x = 0; x < w; ++x) {
+			for(int y = 0; y < h; ++y) {
+				int horp = bHor.getPixel(x,y);
+				int verp = bVer.getPixel(x,y);
+				if(horp != verp){
+					BitmapBiner.setPixel(x,y,0xFF000000);
+				}
+				else
+					BitmapBiner.setPixel(x,y,0xFFFFFFFF);
+			}
+		}
+		return BitmapBiner;
+
+	}
+	public Bitmap DoVerticalFilter(Bitmap BitmapGray){
+		int w, h, threshold;
+
+        h= BitmapGray.getHeight();
+		w= BitmapGray.getWidth();
+
+		threshold = 100;
+
+		Bitmap BitmapBiner = Bitmap.createBitmap(BitmapGray);
+
+		for(int x = 0; x < w; ++x) {
+			for(int y = 0; y < h-1; ++y) {
+
+				int pixel = BitmapGray.getPixel(x, y);
+				int pixelsuivant = BitmapGray.getPixel(x,y+1);
+
+				int gray = (Color.red(pixel) +Color.blue(pixel)+Color.green(pixel))/3;
+				int gNext = (Color.red(pixelsuivant) +Color.blue(pixelsuivant)+Color.green(pixelsuivant))/3;
+				if((gray-gNext)*(gray-gNext)< threshold){
+					BitmapBiner.setPixel(x, y, 0xFFFFFFFF);
+				} else{
+					BitmapBiner.setPixel(x, y, 0xFF000000);
+				}
+
+			}
+		}
+		return BitmapBiner;
+
+	}
+
+    public Bitmap grayScale(Bitmap bmpOriginal)
+    {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
 }
