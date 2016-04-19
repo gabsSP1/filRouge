8fc015ec6fb7db25315217d0884e4071f5f75273
gabsSP1 <gabs.dupont@gmail.com>
2016-04-19 18:05:36 +0200

    integration du reseau en cours

--- app/src/main/java/com/example/photobattle/Connect_activity.java
+++ app/src/main/java/com/example/photobattle/Connect_activity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.andengine.entity.text.Text;
 
 import java.net.Inet6Address;
 import java.net.InetAddress;
